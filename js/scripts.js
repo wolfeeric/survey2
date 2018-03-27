fdc51964d1c7d30eea826dabb2d080be9cab73f1
xerionxavier <phrak9@yahoo.com>
2018-03-26 20:54:14 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $("#mainForm").submit(function(event){
+
+    var userColor = $("input:radio[name=color]:checked").val();
+    var userSound = $("input:radio[name=sound]:checked").val();
+    var userCars = $("input:radio[name=cars]:checked").val();
+    var result = userColor + " " + userSound + " " + userCars;
+
+    //$("#output").text(result).show();
+    //$(".hidden").text(result).show();
+
+    $("#color1").empty().append(userColor);
+    $("#sounds1").empty().append(userSound);
+    $("#cars1").empty().append(userCars);
+    $("#output").show();
+
+    console.log(" USER ENTERED: "+ userColor + " USER ENTERED: " + userSound + " USER ENTERED: " + userCars);
+    event.preventDefault();
+  });
+
+});
