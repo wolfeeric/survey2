6df79e0be5f88ebd6b5dc4c2d1734b66ef79a1eb
xerionxavier <phrak9@yahoo.com>
2018-03-26 21:36:15 -0700

    add more radios

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     var userColor = $("input:radio[name=color]:checked").val();
     var userSound = $("input:radio[name=sound]:checked").val();
     var userCars = $("input:radio[name=cars]:checked").val();
+    var userMusic = $("input:radio[name=music]:checked").val();
     var result = userColor + " " + userSound + " " + userCars;
     console.log(result);
     //$("#output").text(result).show();
@@ -12,6 +13,7 @@ $(document).ready(function(){
     $("#color1").empty().append(userColor);
     $("#sounds1").empty().append(userSound);
     $("#cars1").empty().append(userCars);
+    $("#music1").empty().append(userMusic);
     $("#output").show();
 
     console.log(" USER ENTERED: "+ userColor + " USER ENTERED: " + userSound + " USER ENTERED: " + userCars);
